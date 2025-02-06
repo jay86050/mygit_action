9d7f3f4be478bab0969185576456540a39d0b388
jay86050 <126748762+jay86050@users.noreply.github.com>
2025-02-06 16:18:38 +0530

    Create app.js test 1

---
+++ app.js
@@ -0,0 +1,73 @@
+import * as React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+impob rt LabourVoucher from "./Accounts/LabourVoucher";
+              element={<DataTable columns={columns} rows={rows} />}
+            />
+            <Route
+              path="/WebsiteAutoEnquiry/*"
+              element={<WebsiteAutoEnquiry />}
+            />
+            <Route path="/CustomerFollowUp/*" element={<CustomerFollowUp />} />
+            <Route path="/VIPCustomers/*" element={<VIPCustomers />} />
+            <Route path="/Booking/*" element={<CustomerBookings />} />
+            <Route path="/DemandLetter/*" element={<DemandLetter />} />
+           
+            <Route
+              path="/CustomerBankwiseCurrentDuesReport/*"
+              element={<CustomerBankwiseCurrentDuesReport />}
+            />
+            <Route
+              path="/NoLoanCustomers/*"
+              element={<NoLoanCustomers />}
+            />
+            <Route
+              path="/AllTransactionReport/*"
+              element={<AllTransactionReport />}
+            />
+            <Route
+              path="/PaymentsReceivedforGivenPeriod/*"
+              element={<PaymentsReceivedforGivenPeriod />}
+            />
+            <Route
+              path="/CustomerwiseParkingReport/*"
+              element={<CustomerwiseParkingReport />}
+            />
+            <Route
+              path="/PossessionReport/*"
+              element={<PossessionReport />}
+            />
+            <Route
+              path="/MisReport/*"
+              element={<MisReport />}
+            />
+            <Route
+              path="/ApartmentDeedDoneForParticularPeriod/*"
+              element={<ApartmentDeedDoneForParticularPeriod />}
+            />
+            <Route
+              path="/ApartmentDeedReport/*"
+              element={<ApartmentDeedReport />}
+            />
+            <Route
+              path="/DayEndFollowupReport/*"
+              element={<DayEndFollowupReport />}
+            
+            <Route
+              path="/BalanceSheetReport/*"
+            >
+              <Route index={true} element={<BalanceSheetReport/>}/>
+              <Route 
+                path="getBalanceSheetType/*"
+                element={<BalanceSheetAnnexReport/>}
+              />
+             
+            </Route>
+            <Route path="/AccountExpensesRinku/*" element={<AccountExpensesRinku/>}/>
+          </Routes>
+        </Router>
+      </LocalizationProvider>
+    </>
+  );
+}
+
+export default App;
